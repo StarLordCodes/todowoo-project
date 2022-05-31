5221ce798a2230fbc35f76d8a80fdf6e119700b6
StarLordCodes <amammoottil@outlook.com>
2022-05-31 19:10:17 +0530

    Errors incurring when trying to signup like password mismatching and same username is fixed. We just provide the error message in the same page itself.Also added csrf token in html page to prevent attacks. Next step is authentication of login and redirection.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 def home(request):
@@ -8,7 +10,31 @@ def home(request):
 
 
 def signupuser(request):
-    return render(request, "todo/signupuser.html", {"form": UserCreationForm, "page_name": "Signup page"})
+    # check if it's a get or POST request, POST means we have to create a user from the
+    # submiited UserCreationForm, if its get request we have to provide the form to
+    # the user
+    if request.method == 'GET':
+        return render(request, "todo/signupuser.html", {"form": UserCreationForm, "page_name": "Signup page"})
+    else:
+        # we have to check if user has entered the same password in both the boxes
+        # on inspecting the password and username elements in the boxes on the form
+        # in the browser we learn that the fields are returned as 'username','password1',
+        # 'password2'
+        try:
+            # Here we are using a try except to raise an exception in case of an IntegrityError. First Import
+            # integrityError from django.db then use this block. IntegrityError is raised when an exiting username is
+            # used again.
+
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            else:
+                return render(request, "todo/signupuser.html",
+                              {"form": UserCreationForm, "error_message": "Passwords do not match"})
+        except IntegrityError:
+            return render(request, "todo/signupuser.html",
+                          {"form": UserCreationForm,
+                           "error_message": "Username already taken please use another username"})
 
 
 def loginuser(request):
