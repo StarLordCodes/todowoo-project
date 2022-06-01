b048e128742501b961d3ddebe8d08a81bb6df151
StarLordCodes <amammoottil@outlook.com>
2022-06-01 09:26:37 +0530

    Added the todo data model in models.py and registered it in admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+# adding a class to customise how the admin can see the information in the model
+# here created has automatically generated field value so is not visible, hence
+# we are adding a custom class
+
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    # allows to add model even without content in memo
+    memo = models.TextField(blank=True)
+    # automatically add created date and time
+    created = models.DateTimeField(auto_now_add=True)
+    # allows to create objects even if datecompleted is not filled
+    datecompleted = models.DateTimeField(blank=True, null=True)
+    # set important as False for default
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
